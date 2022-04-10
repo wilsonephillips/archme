58a074cc8d1b596bf815758f283db43ed692402e
Wilson Phillips <88855916+wilsonephillips@users.noreply.github.com>
2022-04-10 09:55:29 -0500

    Add warning to not rename file

--- files/99-disable-touchscreen.sh
+++ files/99-disable-touchscreen.sh
@@ -1,5 +1,6 @@
 # This file must be placed in the following folder
 # /etc/X11/xinit/xinitrc.d/99-disable-touchscreen.sh
+# Do not rename the file
 
 Section "InputClass"
     Identifier  "Wacom HID 4861 Finger"
