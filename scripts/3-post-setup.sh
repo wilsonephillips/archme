a1eba5f0700ab550ab3339054d1964efb5ff05e8
Wilson Phillips <88855916+wilsonephillips@users.noreply.github.com>
2022-04-10 07:02:36 -0500

    Remove Themes

--- scripts/3-post-setup.sh
+++ scripts/3-post-setup.sh
@@ -31,31 +31,18 @@ echo -ne "
   ██║  ██║██║  ██║╚██████╗██║  ██║██║██║██║╚██████╗
   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝╚═╝ ╚═════╝
 ------------------------------------------------------
-             Automated Arch Linux Installer
+            Automated Arch Linux Installer
 ------------------------------------------------------
-          Creating (and Theming) Grub Boot Menu
+               Creating Grub Boot Menu
 ------------------------------------------------------
 "
 # set kernel parameter for decrypting the drive
 if [[ "${FS}" == "luks" ]]; then
 sed -i "s%GRUB_CMDLINE_LINUX_DEFAULT=\"%GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=UUID=${ENCRYPTED_PARTITION_UUID}:ROOT root=/dev/mapper/ROOT %g" /etc/default/grub
 fi
-# set kernel parameter for adding splash screen
-# sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& splash /' /etc/default/grub
 
-# echo -e "Installing CyberRe Grub theme..."
-# THEME_DIR="/boot/grub/themes"
-# THEME_NAME=CyberRe
-# echo -e "Creating the theme directory..."
-# mkdir -p "${THEME_DIR}/${THEME_NAME}"
-# echo -e "Copying the theme..."
-# cd ${HOME}/archme
-# cp -a configs${THEME_DIR}/${THEME_NAME}/* ${THEME_DIR}/${THEME_NAME}
 echo -e "Backing up Grub config..."
 cp -an /etc/default/grub /etc/default/grub.bak
-# echo -e "Setting the theme as the default..."
-# grep "GRUB_THEME=" /etc/default/grub 2>&1 >/dev/null && sed -i '/GRUB_THEME=/d' /etc/default/grub
-# echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" >> /etc/default/grub
 echo -e "Updating grub..."
 grub-mkconfig -o /boot/grub/grub.cfg
 echo -e "All set!"
@@ -69,17 +56,13 @@ echo -ne "
   ██║  ██║██║  ██║╚██████╗██║  ██║██║██║██║╚██████╗
   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝╚═╝ ╚═════╝
 ------------------------------------------------------
-             Automated Arch Linux Installer
+            Automated Arch Linux Installer
 ------------------------------------------------------
      Enabling (and Theming) Login Display Manager
 ------------------------------------------------------
 "
 if [[ ${DESKTOP_ENV} == "kde" ]]; then
   systemctl enable sddm.service
-#  if [[ ${INSTALL_TYPE} == "FULL" ]]; then
-#    echo [Theme] >>  /etc/sddm.conf
-#    echo Current=Nordic >> /etc/sddm.conf
-#  fi
 
 elif [[ "${DESKTOP_ENV}" == "gnome" ]]; then
   systemctl enable gdm.service
@@ -89,12 +72,6 @@ elif [[ "${DESKTOP_ENV}" == "lxde" ]]; then
 
 elif [[ "${DESKTOP_ENV}" == "openbox" ]]; then
   systemctl enable lightdm.service
-#  if [[ "${INSTALL_TYPE}" == "FULL" ]]; then
-#    # Set default lightdm-webkit2-greeter theme to Litarvan
-#    sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = litarvan #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf
-#    # Set default lightdm greeter to lightdm-webkit2-greeter
-#    sed -i 's/#greeter-session=example.*/greeter-session=lightdm-webkit2-greeter/g' /etc/lightdm/lightdm.conf
-#  fi
 
 else
   if [[ ! "${DESKTOP_ENV}" == "server"  ]]; then
@@ -133,62 +110,6 @@ systemctl enable bluetooth
 echo "  Bluetooth enabled"
 systemctl enable avahi-daemon.service
 echo "  Avahi enabled"
-
-# if [[ "${FS}" == "luks" || "${FS}" == "btrfs" ]]; then
-# echo -ne "
-------------------------------------------------------
-   █████╗ ██████╗  ██████╗██╗  ██╗ ██████╗  ██████╗
-  ██╔══██╗██╔══██╗██╔════╝██║  ██║██╔██╔██╗██╔════╝
-  ███████║██████╔╝██║     ███████║██║██║██║██████╗ 
-  ██╔══██║██╔══██╗██║     ██╔══██║██║██║██║██╔═══╝ 
-  ██║  ██║██║  ██║╚██████╗██║  ██║██║██║██║╚██████╗
-  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝╚═╝ ╚═════╝
-------------------------------------------------------
-             Automated Arch Linux Installer
-------------------------------------------------------
-
-               Creating Snapper Config
-------------------------------------------------------
-"
-
-# SNAPPER_CONF="$HOME/archme/configs/etc/snapper/configs/root"
-# mkdir -p /etc/snapper/configs/
-# cp -rfv ${SNAPPER_CONF} /etc/snapper/configs/
-
-# SNAPPER_CONF_D="$HOME/archme/configs/etc/conf.d/snapper"
-# mkdir -p /etc/conf.d/
-# cp -rfv ${SNAPPER_CONF_D} /etc/conf.d/
-
-# fi
-
-# echo -ne "
-# ------------------------------------------------------
-#     █████╗ ██████╗  ██████╗██╗  ██╗ ██████╗  ██████╗
-#   ██╔══██╗██╔══██╗██╔════╝██║  ██║██╔██╔██╗██╔════╝
-#   ███████║██████╔╝██║     ███████║██║██║██║██████╗ 
-#   ██╔══██║██╔══██╗██║     ██╔══██║██║██║██║██╔═══╝ 
-#   ██║  ██║██║  ██║╚██████╗██║  ██║██║██║██║╚██████╗
-#   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝╚═╝ ╚═════╝
-# ------------------------------------------------------
-#              Automated Arch Linux Installer
-# ------------------------------------------------------
-
-#       Enabling (and Theming) Plymouth Boot Splash
-# ------------------------------------------------------
-# "
-# PLYMOUTH_THEMES_DIR="$HOME/archme/configs/usr/share/plymouth/themes"
-# PLYMOUTH_THEME="arch-glow" # can grab from config later if we allow selection
-# mkdir -p /usr/share/plymouth/themes
-# echo 'Installing Plymouth theme...'
-# cp -rf ${PLYMOUTH_THEMES_DIR}/${PLYMOUTH_THEME} /usr/share/plymouth/themes
-# if  [[ $FS == "luks"]]; then
-#   sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
-#   sed -i 's/HOOKS=(base udev \(.*block\) /&plymouth-/' /etc/mkinitcpio.conf # create plymouth-encrypt after block hook
-# else
-#   sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
-# fi
-# plymouth-set-default-theme -R arch-glow # sets the theme and runs mkinitcpio
-# echo 'Plymouth theme installed'
 
 echo -ne "
 ------------------------------------------------------
