0ca70cdb25acc60d05bc23fffc6bad68ef4fa959
Wilson Phillips <88855916+wilsonephillips@users.noreply.github.com>
2022-06-17 06:42:34 -0500

    Remove 1MB partition for BIOS

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -47,9 +47,9 @@ sgdisk -Z ${DISK} # zap all on disk
 sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
 
 # create partitions
-sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
-sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
-sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, remaining
+# sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
+sgdisk -n 1::+500M --typecode=1:ef00 --change-name=1:'EFIBOOT' ${DISK} # partition 1 (UEFI Boot Partition)
+sgdisk -n 2::-0 --typecode=3:8300 --change-name=2:'ROOT' ${DISK} # partition 2 (Root), default start, remaining
 if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
     sgdisk -A 1:set:2 ${DISK}
 fi
@@ -96,17 +96,17 @@ subvolumesetup () {
 }
 
 if [[ "${DISK}" =~ "nvme" ]]; then
+    partition1=${DISK}p1
     partition2=${DISK}p2
-    partition3=${DISK}p3
 else
     partition2=${DISK}2
     partition3=${DISK}3
 fi
 
 if [[ "${FS}" == "btrfs" ]]; then
-    mkfs.fat -F32 -n "EFIBOOT" ${partition2}
-    mkfs.btrfs -L ROOT ${partition3} -f
-    mount -t btrfs ${partition3} /mnt
+    mkfs.fat -F32 -n "EFIBOOT" ${partition1}
+    mkfs.btrfs -L ROOT ${partition2} -f
+    mount -t btrfs ${partition2} /mnt
     subvolumesetup
 elif [[ "${FS}" == "ext4" ]]; then
     mkfs.fat -F32 -n "EFIBOOT" ${partition2}
